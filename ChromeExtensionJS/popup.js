c37119c05efa7b74cadb0a61627c68d18115f974
matttang7 <mt13@illinois.edu>
2021-12-07 17:49:23 -0600

    finishing touches to make project look cleaner

--- ChromeExtensionJS/popup.js
+++ ChromeExtensionJS/popup.js
@@ -14,7 +14,31 @@ $(function(){
 				.then(response =>  response.json())
 				.then(json => {
 					// console.log('parsed json', json) // access json.body here
-					document.getElementById("p1").innerHTML = JSON.stringify(json,null, 2);
+					json['results'].forEach(element => {
+						const split = element['name'].split('##')
+						element['timestamp'] = split[0]
+						element['courseraLink'] = split[1]
+					});
+					document.getElementById("p1").innerHTML = "Query: " + json['query'];
+					document.getElementById("p2").innerHTML = "Elapsed Time: " + json['elapsed_time'];
+
+					function loadTableData(items) {
+						const table = document.getElementById("testBody");
+						for(var i = table.rows.length - 1; i > 0; i--)
+						{
+							table.deleteRow(i);
+						}
+						items.forEach( item => {
+						  let row = table.insertRow();
+						  let content = row.insertCell(0);
+						  content.innerHTML = item.content;
+						  let timestamp = row.insertCell(1);
+						  timestamp.innerHTML = item.timestamp;
+						  let courseraLink = row.insertCell(2);
+						  courseraLink.innerHTML = item.courseraLink;
+						});
+					  }
+					loadTableData(json['results']);
 				})
 				.catch(error => console.log(error));
 
